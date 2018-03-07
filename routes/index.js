8d72716ccb2679623a551170b1e529e88ed3735e
I-CCEW <icepentium@Gmail.com>
2018-03-06 20:53:02 -0600

    edit .gitignore and create cart.html

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/cart',function(req, res, next){
+  res.redirect('../Cart.html');
+});
 module.exports = router;
