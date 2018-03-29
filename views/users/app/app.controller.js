d0c019dc66e45672e9dff45d285052c979fecda4
Lihua <icepentium@Gmail.com>
2018-03-28 21:27:14 -0500

    modified signup page, add angular, in processing

---
+++ views/users/app/app.controller.js
@@ -0,0 +1,19 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('User')
+        .controller('Matchcheck', Matchcheck);
+
+    ControllerController.$inject = ['dependency1'];
+    function ControllerController(dependency1) {
+        var vm = this;
+        
+
+        activate();
+
+        ////////////////
+
+        function activate() { }
+    }
+})();
