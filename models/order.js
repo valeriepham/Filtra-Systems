cf149c317341ab568bdc9f0d95fb03524f26b6e2
Michael Fedell <s.michaelfedell@gmail.com>
2018-04-10 16:21:17 -0500

    Added Orders to User Profile

--- models/order.js
+++ models/order.js
@@ -2,19 +2,20 @@ let mongoose = require('mongoose');
 
 let orderSchema = mongoose.Schema({
   user: {type: mongoose.Schema.Types.ObjectId, required: false, ref: 'User'},
-  cart: {type: Object, required: true},
+  cart: {type: Object, required: true, default: null},
   shippingAddress: {
-    street: {type: String, required: true},
-    state: {type: String, required: true},
-    zip: {type: Number, required: true}
+    street: {type: String, required: true, default: 'Test Street'},
+    state: {type: String, required: true, default: 'Test State'},
+    zip: {type: Number, required: true, default: 11111}
   },
   billingAddress: {
-    street: {type: String, required: true},
-    state: {type: String, required: true},
-    zip: {type: Number, required: true}
+    street: {type: String, required: true, default: 'Test Street2'},
+    state: {type: String, required: true, default: 'Test State2'},
+    zip: {type: Number, required: true, default: 22222}
   },
-  name: {type: String, required: true},
-  paymentId: {type: String, required: true}
+  name: {type: String, required: true, default: 'Test Name'},
+  paymentId: {type: String, required: true, default: 'Test PaymentID'},
+  date: { type: Date, default: Date.now },
 });
 
 let Order = mongoose.model('Order', orderSchema);
